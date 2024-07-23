d85df3637b9a209a4737f4c4183511f141847378
Archana G S <62071221+archanags001@users.noreply.github.com>
2024-07-23 10:39:30 -0700

    Update contact_form.py

--- contact_form.py
+++ contact_form.py
@@ -36,45 +36,10 @@ def contact():
 
     components.html(
         f"""
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScBhBAdKhnAnlQ0lKU65Fz_RQ8Tkf_E8UMPbJkXTQJDFCoOvg/viewform?embedded=true" width="640" height="689" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLaMWyScjbqoo6I5w5MtoQwfSU-Izghn1y_jsTP-yuf5zZOA/viewform?embedded=true" width="640" height="741" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
         """,
         height=1800,
     )
-# def contact():
-#     col1, col2 = st.columns(2)
 
-#     # col1.markdown("## Experience")
-#     with col1:
-#         st.markdown("""
-#                     <style>
-#                     .centered {
-#                         display: flex;
-#                         align-items: center;
-#                         height: 100%;
-#                         margin-top: 200px; /* Adjust this value as needed */
-#                     }
-#                     </style>
-#                     <div class="centered">
-#                         <h2>📨 Contact Form </h2>
-#                     </div>
-#                 """, unsafe_allow_html=True)
-#     path = "Animation_contact.json"
-#     with open(path, "r") as file:
-#         url = json.load(file)
-#     with col2:
-#         st_lottie(url,
-#                   reverse=True,
-#                   height=400,
-#                   width=400,
-#                   speed=1,
-#                   loop=True,
-#                   quality='high',
-#                   )
-#    components.html(
-#     f"""
-# <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLaMWyScjbqoo6I5w5MtoQwfSU-Izghn1y_jsTP-yuf5zZOA/viewform?embedded=true" width="640" height="741" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-    
-#     """,
-#     height=1800,
-# )
 
